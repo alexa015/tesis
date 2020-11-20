7fba7d2fe450f6bf4c72a3d8b5bddae790159b56
Alexa <alexa.barrios@konecta.com.py>
2020-11-20 17:29:50 -0300

    cambios

--- hotel/registro_reservas.php
+++ hotel/registro_reservas.php
@@ -2,20 +2,20 @@
 //se establece una conexion a la base de datos
 include 'conexion.php';
 //si se han enviado datos
-$nombre = (isset($_POST['registrar_nombre']));
-$apellido = (isset($_POST['registrar_apellido']));
-$email = (isset($_POST['registrar_email']));
-$telefono = (isset($_POST['registrar_telefono']));
-$fech_ingreso = (isset($_POST['registrar_fech_ingreso']));
-$fech_salida = (isset($_POST['registrar_fech_salida']));
-$cant_personas = (isset($_POST['registrar_cant_personas']));
-$tipo_hab = (isset($_POST['registrar_tipo_hab']));
-$nro_hab = (isset($_POST['registrar_nro_hab']));
-$cant_dias = (isset($_POST['registrar_cant_dias']));
+$nombre = $_POST['registrar_nombre'];
+$apellido = $_POST['registrar_apellido'];
+$email = $_POST['registrar_email'];
+$telefono = $_POST['registrar_telefono'];
+$fech_ingreso = $_POST['registrar_fech_ingreso'];
+$fech_salida = $_POST['registrar_fech_salida'];
+$cant_personas = $_POST['registrar_cant_personas'];
+$tipo_hab = $_POST['registrar_tipo_hab'];
+$nro_hab = $_POST['registrar_nro_hab'];
+$cant_dias = $_POST['registrar_cant_dias'];
 
 $check="SELECT * FROM reservaciones WHERE email = '$email'";
 $rs = mysqli_query($conexion,$check);
-$data = mysqli_fetch_array($rs, MYSQLI_NUM);
+$data = mysqli_fetch_array($rs);
 
 //var_dump($newUser) or die();
 
@@ -25,7 +25,7 @@ if($data[0] > 1) {
 } else {
 	$newUser="INSERT INTO reservaciones (nombre, apellido, email, telefono, fech_ingreso, fech_salida, cant_personas, tipo_hab, nro_hab, cant_dias) VALUES ('$nombre','$apellido','$email','$telefono','$fech_ingreso','$fech_salida','$cant_personas','$tipo_hab','$nro_hab','$cant_dias')";
 	if (mysqli_query($conexion,$newUser)) {
-		echo "<script>alert('Error');
+		echo "<script>alert('La habitación fue reservada con éxito!');
 		window.location='/tesis/hotel/mis_reservas.php'</script>";
 											
 	}else {
